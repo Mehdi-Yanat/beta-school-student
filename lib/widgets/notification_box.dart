62f23ca0018dac7283033d13c3ad9ed4c8946b7c
Mehdi Yanat <hamar3106@gmail.com>
2024-12-15 13:00:21 +0100

    init project

---
+++ lib/widgets/notification_box.dart
@@ -0,0 +1,50 @@
+import 'package:badges/badges.dart' as badge_lib;
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:online_course/theme/color.dart';
+
+class NotificationBox extends StatelessWidget {
+  NotificationBox({
+    Key? key,
+    this.onTap,
+    this.size = 5,
+    this.notifiedNumber = 0,
+  }) : super(key: key);
+
+  final GestureTapCallback? onTap;
+  final int notifiedNumber;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(size),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: AppColor.appBarColor,
+          border: Border.all(
+            color: Colors.grey.withOpacity(.3),
+          ),
+        ),
+        child: notifiedNumber > 0 ? _buildIconNotified() : _buildIcon(),
+      ),
+    );
+  }
+
+  Widget _buildIconNotified() {
+    return badge_lib.Badge(
+      badgeContent: Text('5'),
+      child: Icon(Icons.notifications),
+    );
+  }
+
+  Widget _buildIcon() {
+    return SvgPicture.asset(
+      "assets/icons/bell.svg",
+      width: 20,
+      height: 20,
+    );
+  }
+}
